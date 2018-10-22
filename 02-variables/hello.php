fc9fc0a659118170357c42ef0a7a6732ed4a6cdc
William DUPUIS <william.dupuis@tuxtelecom.net>
2018-10-22 10:22:35 +0200

    Add variables

---
+++ 02-variables/hello.php
@@ -0,0 +1,9 @@
+<?php
+
+$firstName = 'Matthieu';
+
+echo '<h1>Bonjour ' . $firstName . '</h1>';
+echo "<h1>Bonjour " . $firstName . "</h1>";
+echo '<h1>Bonjour $firstName</h1>';
+// Interpolation de variable possible avec les doubles quotes
+echo "<h1>Bonjour $firstName</h1>";
